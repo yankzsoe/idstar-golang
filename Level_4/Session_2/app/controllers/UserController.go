f662ca4ab150338b9ada7e03bcecb293eaac298c
yankzsoe <yankzsoe@gmail.com>
2023-04-06 23:03:10 +0700

    Level4-session2- add GIN Validation

--- Level_4/Session_2/app/controllers/UserController.go
+++ Level_4/Session_2/app/controllers/UserController.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CreateUserRequest struct {
+	Name     string `json:"name" binding:"required,min=3"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
 type UserController struct{}
 
 func (ctrl *UserController) GetUser(ctx *gin.Context) {
@@ -14,6 +20,11 @@ func (ctrl *UserController) GetUser(ctx *gin.Context) {
 }
 
 func (ctrl *UserController) PostUser(ctx *gin.Context) {
-	// Create new user
-	ctx.JSON(http.StatusCreated, "OK")
+	req := CreateUserRequest{}
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Successfully"})
 }
