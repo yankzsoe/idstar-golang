3aa0e3a24ef733b627730cd76f9eca8f0c62ec75
yankzsoe <yankzsoe@gmail.com>
2023-04-08 12:22:58 +0700

    Level 4 - Session 4 - Middleware

---
+++ Level_4/Session_4/app/routers/Router.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	controller "idstar.com/session4/app/controllers"
+	middleware "idstar.com/session4/app/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// Add Middleware
+	logger := middleware.LoggerMiddleware{}
+	r.Use(logger.Logger())
+
+	// Load controller
+	userCtrl := new(controller.UserController)
+
+	// Create group routing endpoint "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		employee := v1.Group("/user")
+		{
+			employee.GET("/:id", userCtrl.GetUser)
+			employee.POST("", userCtrl.PostUser)
+		}
+
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	return r
+}
---
+++ Level_4/Session_4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"idstar.com/session4/app/routers"
+
+	"idstar.com/session4/docs"
+)
+
+func main() {
+	r := routers.SetupRouter()
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger User API"
+	docs.SwaggerInfo.Description = "This is a sample Swagger in Golang with GIN Framework."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "127.0.0.1:5001/api"
+	docs.SwaggerInfo.BasePath = "/v1"
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	r.Run(":5001")
+}
